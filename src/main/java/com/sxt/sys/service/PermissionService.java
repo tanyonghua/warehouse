a2cd68f37dab3f31a02aae42de3a4e59e4f964f6
tanyonghua <765059825@qq.com>
2020-08-11 22:37:07 +0800

    家里

---
+++ src/main/java/com/sxt/sys/service/PermissionService.java
@@ -0,0 +1,16 @@
+package com.sxt.sys.service;
+
+import com.sxt.sys.domain.Permission;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author Mr谭
+ * @since 2020-08-11
+ */
+public interface PermissionService extends IService<Permission> {
+
+}
