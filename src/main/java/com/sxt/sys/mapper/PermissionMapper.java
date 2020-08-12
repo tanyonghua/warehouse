133c1e5ac4c8998dd50d663dfdd6a979569fd79f
谭永华 <765059825@qq.com>
2020-08-12 08:44:12 +0800

    第二次提交

---
+++ src/main/java/com/sxt/sys/mapper/PermissionMapper.java
@@ -0,0 +1,16 @@
+package com.sxt.sys.mapper;
+
+import com.sxt.sys.domain.Permission;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Mr谭
+ * @since 2020-08-11
+ */
+public interface PermissionMapper extends BaseMapper<Permission> {
+
+}
---
+++ src/main/java/com/sxt/sys/service/impl/PermissionServiceImpl.java
@@ -0,0 +1,20 @@
+package com.sxt.sys.service.impl;
+
+import com.sxt.sys.domain.Permission;
+import com.sxt.sys.mapper.PermissionMapper;
+import com.sxt.sys.service.PermissionService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author Mr谭
+ * @since 2020-08-11
+ */
+@Service
+public class PermissionServiceImpl extends ServiceImpl<PermissionMapper, Permission> implements PermissionService {
+
+}
